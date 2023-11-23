dbf763dfd4e92c27359e2d7a29778be7b99d8e7c
valyy151 <marin.valenta1@gmail.com>
2023-11-23 10:31:36 +0100

    new progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,12 @@ import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import { cookies } from 'next/headers'
 
-import NextTopLoader from 'nextjs-toploader'
-
 import { TRPCReactProvider } from '@/trpc/react'
 import { ThemeProvider } from './_components/theme-provider'
 import { Toaster } from '@/app/_components/ui/toaster'
 
 import Navbar from './_components/navbar'
+import ProgressBar from './_components/progress-bar'
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -29,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				<TRPCReactProvider cookies={cookies().toString()}>
 					<ThemeProvider attribute='class'>
 						<Navbar />
-						<NextTopLoader showSpinner={false} />
+						<ProgressBar />
 						{children}
 						<Toaster />
 					</ThemeProvider>
